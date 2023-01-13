cb106c318e99c55af108b30c6d05b5de9be0eb93
AJAguirre <aguaguirre@hotmail.com>
2023-01-13 15:34:43 -0300

    Mita de ejercicio 5

---
+++ prueba05.js
@@ -0,0 +1,32 @@
+// var numeros = [1, 2, 3, 4, 5, 6, 7]
+
+// console.log(numeros[4]);
+
+// numeros.pop();
+
+// console.log(numeros);
+
+// numeros.push(6);
+
+// console.log(numeros);
+// console.log(numeros.length);
+
+// var masUno =numeros.map((num) => {
+//     return num + 1;})
+
+//     console.log(masUno); 
+var arrayOfNums = [1, 2, 3, 4, 5, 6, 7]
+
+console.log(arrayOfNums);   
+
+    // function agregarNumeros(arrayOfNums) {
+    //     // El parámetro "arrayOfNums" debe ser un arreglo de números.
+    //     // Suma todos los elementos y retorna el resultado.
+    //     // Tu código:
+        
+    //     // var suma = arrayOfNums [0] + arrayOfNums [1] + arrayOfNums [2] + arrayOfNums [3] + arrayOfNums [4];
+    //     console.log(arrayOfNums);   
+    //     // return suma; 
+
+        
+    
